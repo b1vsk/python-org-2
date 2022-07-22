c5816850ffcfbad6f793317ba0e3c2b3a0576880
João <109762776+b1vsk@users.noreply.github.com>
2022-07-22 13:36:05 -0300

    Add files via upload

---
+++ js/dark-theme.js
@@ -0,0 +1,5 @@
+function darkTheme() {
+	document.body.style.backgroundColor = "#1A2128";
+	document.getElementById("paragraph-main").style.color = "#FFF";
+	document.getElementById("title-main").style.color = "#FFF";
+}
